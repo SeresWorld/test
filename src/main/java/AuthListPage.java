c046d24d3f73b52ce51a63c45b614461bb02608d
SeresWorld <73892596+SeresWorld@users.noreply.github.com>
2022-12-02 13:35:34 +0600

    add steps

--- src/main/java/AuthListPage.java
+++ src/main/java/AuthListPage.java
@@ -1,6 +1,7 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.qameta.allure.Step;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -23,37 +24,38 @@ public class AuthListPage extends PageBase {
             ("//XCUIElementTypeApplication[@name=\"BCC.KZ\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeTextField");
     private By input_pass_ios = By.cssSelector("XCUIElementTypeSecureTextField[value='Введите пароль']");
     private By button_permission_ios = By.name("Однократно");
+    @Step ("Клик по кнопке Войти на экране сплеша")
     public void sign_main_button_click() {
         click((MobileElement) driver.findElement(sign_button));
     }
 
     public void sign_main_button_click_ios() {click((MobileElement) driver.findElement(sign_button_ios));}
-
+    @Step ("Ввод телефона {login} в поле телефона")
     public void setInput_login(String login) {
         MobileElement input = (MobileElement) driver.findElement(input_login);
         sendText(input, login);
     }
-
+    @Step
     public void setInput_login_ios(String login) {
         MobileElement input = (MobileElement) driver.findElement(input_login_ios);
         sendText(input, login);
     }
-
+    @Step ("Клик по кнопке Войти на экране авторизации")
     public void sign_complete_button_click() {click((MobileElement) driver.findElement(button_login));}
-
+    @Step
     public void sign_complete_button_click_ios() {click((MobileElement) driver.findElement(sign_button_ios));}
 
-
+    @Step ("Ввод пароля {pass} в поле пароля")
     public void setInput_pass(String pass) {
         MobileElement input = (MobileElement) driver.findElement(input_pass);
         sendText(input, pass);
     }
-
+    @Step
     public void setInput_pass_ios(String pass) {
         MobileElement input = (MobileElement) driver.findElement(input_pass_ios);
         sendText(input, pass);
     }
-
+    @Step ("Попытка клика по кнопке разрешения на получение геолокации")
     public void try_button_permission_click() {
         try {
             click((MobileElement) driver.findElement(button_permission));
@@ -62,17 +64,17 @@ public class AuthListPage extends PageBase {
         }
 
     }
-
+    @Step
     public void try_button_permission_click_ios() {
         MobileElement btn = (MobileElement) driver.findElement(button_permission_ios);
         waitForVisability(btn);
         click(btn);
     }
-
+    @Step ("Проверка наличия текста {text} на модалке")
     public void check_text_android_message(String text) {
         containsmessageAssert((MobileElement) driver.findElement(android_message), text);
     }
-
+    @Step ("Проверка неактивности кнопки Войти на экране авторизации")
     public void check_active_sign_button_is_not_clickable() {
         String button_login_atr = getAttribute((MobileElement) driver.findElement(button_login), "enabled");
         Assert.assertEquals("Object is clickable", "false", button_login_atr);
