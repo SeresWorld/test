bb5ef82a71bd47fbdeaa8a95a496bc8549a3c79b
SeresWorld <73892596+SeresWorld@users.noreply.github.com>
2023-01-27 09:11:16 +0600

    test

--- src/test/java/pages/login/AuthListPage.java
+++ src/test/java/pages/login/AuthListPage.java
@@ -48,7 +48,7 @@ public class AuthListPage extends PageBase {
     @FindAll(
             {
                     @FindBy(xpath = "//XCUIElementTypeButton[@name=\"Войти\"]"),
-                    @FindBy(id = "kz.bcc.starbanking.stage:id/fragment_login_enter")
+                    @FindBy(id = "kz.bcc.starbanking.stage:id/fragment_login_enter")dasdasd
             }
     ) protected WebElement buttonLogin;
 
